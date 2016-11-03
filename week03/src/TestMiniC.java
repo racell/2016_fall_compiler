3c459aa7d1ca82f6a0481a4f75b0a06d4ec28989
racell <fjunkimsj@gmail.com>
2016-11-03 17:15:34 +0900

    Add test case

--- week03/src/TestMiniC.java
+++ week03/src/TestMiniC.java
@@ -1,5 +1,21 @@
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.IOException;
+
 /**
  * Created by SeongJung on 2016-11-03.
  */
 public class TestMiniC {
+    public static void main(String[] args) throws IOException{
+        MiniCLexer lexer = new MiniCLexer(new ANTLRFileStream("test"));
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        MiniCParser parser = new MiniCParser(tokenStream);
+        ParseTree tree = parser.program();
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(new MiniCBaseListener(), tree);
+    }
 }
