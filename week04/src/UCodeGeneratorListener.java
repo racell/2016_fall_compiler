e0aa3d6ad5d7416cde3591c4afd2c203f8870b88
racell <fjunkimsj@gmail.com>
2016-12-07 00:57:30 +0900

    Add some exit method & Fix test file

--- week04/src/UCodeGeneratorListener.java
+++ week04/src/UCodeGeneratorListener.java
@@ -1,7 +1,6 @@
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
 
 import java.util.HashMap;
-import java.util.StringTokenizer;
 
 /**
  * Created by SeongJung on 2016-11-29.
@@ -11,17 +10,26 @@ public class UCodeGeneratorListener extends MiniCBaseListener {
     final private String blank = "           ";
     private int label;
     private HashMap<String, String> localMap = new HashMap<>();
-    private int localVar_offset = 0;
+    private HashMap<String, String> externalMap = new HashMap<>();
+    private int localVarOffset = 0;
+    private int externalVarOffset = 0;
 
     @Override
     public void exitProgram(MiniCParser.ProgramContext ctx) {
-        for (int i = 0; i < ctx.decl().size(); i++) {
-            System.out.println(newTexts.get(ctx.decl(i)));
+        int i = 0;
+        while (ctx.decl(i).var_decl() != null) {
+            System.out.print(newTexts.get(ctx.decl(i)));
+            i++;
         }
-        System.out.println("bgn 0");
-        System.out.println("ldp");
-        System.out.println("call main");
-        System.out.println("end");
+        while (i < ctx.decl().size()) {
+            System.out.print(newTexts.get(ctx.decl(i)));
+            i++;
+        }
+        System.out.println();
+        System.out.println(blank + "bgn " + externalMap.size());
+        System.out.println(blank + "ldp");
+        System.out.println(blank + "call main");
+        System.out.println(blank + "end");
     }
 
     @Override
@@ -35,52 +43,51 @@ public class UCodeGeneratorListener extends MiniCBaseListener {
 
     @Override
     public void exitVar_decl(MiniCParser.Var_declContext ctx) {
-        String type, s1, eq, colon, s2;
-        type = newTexts.get(ctx.type_spec());
+        String s1;
         s1 = ctx.getChild(1).getText();
         if (isDeclaration(ctx)) {
-            colon = ctx.getChild(2).getText();
-            newTexts.put(ctx, type + " " + s1 + colon);
+            externalMap.put(s1, "1 " + ++externalVarOffset);
+            newTexts.put(ctx, blank + "sym " + externalMap.get(s1) + " 1");
         } else if (isDeclarationAndDefine(ctx)) {
-            eq = ctx.getChild(2).getText();
-            s2 = ctx.getChild(3).getText();
-            colon = ctx.getChild(4).getText();
-            newTexts.put(ctx, type + " " + s1 + " " + eq + " " + s2 + colon);
+            externalMap.put(s1, "1 " + ++externalVarOffset);
+            newTexts.put(ctx, blank + "sym " + externalMap.get(s1) + " 1\nldc " + ctx.getChild(3).getText() + "\nstr " + externalMap.get(s1));
         } else if (isArrayDeclaration(ctx)) {
-            eq = ctx.getChild(2).getText();
-            s2 = ctx.getChild(3).getText() + ctx.getChild(4).getText();
-            colon = ctx.getChild(5).getText();
-            newTexts.put(ctx, type + " " + s1 + eq + s2 + colon);
+            externalMap.put(s1, "1 " + ++externalVarOffset);
+            externalVarOffset += Integer.parseInt(ctx.getChild(3).getText()) - 1;
+            newTexts.put(ctx, blank + "sym " + externalMap.get(s1) + ctx.getChild(3).getText());
         }
     }
 
     @Override
+    public void exitType_spec(MiniCParser.Type_specContext ctx) {
+        newTexts.put(ctx, "");
+    }
+
+    @Override
     public void exitFun_decl(MiniCParser.Fun_declContext ctx) {
-        String type, s1, left, s2, right, s3;
-        type = newTexts.get(ctx.type_spec());
+        String s1, s2, s3;
         s1 = ctx.getChild(1).getText();
-        left = ctx.getChild(2).getText();
         s2 = newTexts.get(ctx.params());
-        right = ctx.getChild(4).getText();
         s3 = newTexts.get(ctx.compound_stmt());
-        newTexts.put(ctx, type + " " + s1 + left + s2 + right + "\n" + s3);
+        newTexts.put(ctx,  s1 + blank.substring(0, blank.length() - s1.length()) + "proc " + localVarOffset + " 2 2" + s2 + "\n" +
+                s3 + blank + "end");
+        localVarOffset = 0;
     }
 
     @Override
     public void exitParams(MiniCParser.ParamsContext ctx) {
-        String s1, type;
+        String s1;
         if (isEmptyParams(ctx)) {
             newTexts.put(ctx, "");
         } else if (isVoidParams(ctx)) {
-            type = ctx.getChild(0).getText();
-            newTexts.put(ctx, type);
+            newTexts.put(ctx, "");
         } else if (isOneParams(ctx)) {
             s1 = newTexts.get(ctx.param(0));
             newTexts.put(ctx, s1);
         } else if (isMultipleParams(ctx)) {
             s1 = newTexts.get(ctx.param(0));
             for (int i = 1; i < ctx.param().size(); i++) {
-                s1 = s1 + ", " + newTexts.get(ctx.param(i));
+                s1 += newTexts.get(ctx.param(i)) + "\n";
             }
             newTexts.put(ctx, s1);
         }
@@ -88,15 +95,15 @@ public class UCodeGeneratorListener extends MiniCBaseListener {
 
     @Override
     public void exitParam(MiniCParser.ParamContext ctx) {
-        String type, s1, left, right;
-        type = newTexts.get(ctx.type_spec());
+        String s1;
         s1 = ctx.getChild(1).getText();
         if (isArrayParam(ctx)) {
-            left = ctx.getChild(2).getText();
-            right = ctx.getChild(3).getText();
-            newTexts.put(ctx, type + " " + s1 + left + right);
+            localMap.put(s1, "2 " + ++localVarOffset);
+            localVarOffset += Integer.parseInt(ctx.getChild(3).getText()) - 1;
+            newTexts.put(ctx, blank + "sym " + localMap.get(s1) + ctx.getChild(3).getText());
         } else {
-            newTexts.put(ctx, type + " " + s1);
+            localMap.put(s1, "2 " + ++localVarOffset);
+            newTexts.put(ctx, blank + "sym " + localMap.get(s1) + " 1");
         }
     }
 
@@ -130,16 +137,16 @@ public class UCodeGeneratorListener extends MiniCBaseListener {
 
     @Override
     public void exitWhile_stmt(MiniCParser.While_stmtContext ctx) {
-        String word, left, right, s1, s2;
-        word = ctx.getChild(0).getText();
-        left = ctx.getChild(1).getText();
+        String word, s1, s2;
         s1 = newTexts.get(ctx.expr());
-        right = ctx.getChild(3).getText();
         s2 = newTexts.get(ctx.stmt());
-        if (newTexts.get(ctx.stmt().compound_stmt()) == null) {
-            s2 = "{\n...." + s2 + "\n}";
-        }
-        newTexts.put(ctx, word + " " + left + s1 + right + "\n" + s2);
+        newTexts.put(ctx, "$$" + label + blank.substring(0, blank.length() - 2 - Integer.toString(label).length()) + "nop" + "\n" +
+                s1 + "\n" +
+                blank + "fjp $$" + (label + 1) + "\n" +
+                s2 +
+                blank + "ujp $$" + label + "\n" +
+                "$$" + (label + 1) + blank.substring(0, blank.length() - 2 - Integer.toString(label).length()) + "nop");
+        label++;
     }
 
     @Override
@@ -159,15 +166,15 @@ public class UCodeGeneratorListener extends MiniCBaseListener {
         String s1;
         s1 = ctx.getChild(1).getText();
         if (isDeclaration(ctx)) {
-            localMap.put(s1, ++localVar_offset + " 1");
-            newTexts.put(ctx, "sym 2 " + localMap.get(s1));
+            localMap.put(s1, "2 " + ++localVarOffset);
+            newTexts.put(ctx, blank + "sym " + localMap.get(s1) + " 1");
         } else if (isDeclarationAndDefine(ctx)) {
-            localMap.put(s1, ++localVar_offset + " 1");
-            newTexts.put(ctx, "sym 2 " + localMap.get(s1) + "\nldc " + ctx.getChild(3).getText() + "\nstr " + localMap.get(s1));
+            localMap.put(s1, "2 " + ++localVarOffset);
+            newTexts.put(ctx, blank + "sym " + localMap.get(s1) + " 1\nldc " + ctx.getChild(3).getText() + "\nstr " + localMap.get(s1));
         } else if (isArrayDeclaration(ctx)) {
-            localMap.put(s1, ++localVar_offset + ctx.getChild(3).getText());
-            localVar_offset += Integer.parseInt(ctx.getChild(3).getText()) - 1;
-            newTexts.put(ctx, "sym 2 " + localMap.get(s1));
+            localMap.put(s1, "2 " + ++localVarOffset);
+            localVarOffset += Integer.parseInt(ctx.getChild(3).getText()) - 1;
+            newTexts.put(ctx, blank + "sym " + localMap.get(s1) + ctx.getChild(3).getText());
         }
     }
 
@@ -178,9 +185,9 @@ public class UCodeGeneratorListener extends MiniCBaseListener {
         s2 = newTexts.get(ctx.stmt(0));
         if (ctx.getChildCount() == 7) {
             s3 = newTexts.get(ctx.stmt(1));
-            newTexts.put(ctx, s1 + "\n" + blank + "fjp $$" + label + s2 + "\n$$" + label + blank.substring(0, blank.length() - 2 - Integer.toString(label).length()) + " nop\n" + s3);
+            newTexts.put(ctx, s1 + "\n" + blank + "fjp $$" + label + "\n" + s2 + "\n$$" + label + blank.substring(0, blank.length() - 2 - Integer.toString(label).length()) + "nop\n" + s3);
         } else {
-            newTexts.put(ctx, s1 + "\n" + blank + "fjp $$" + label + s2 + "\n$$" + label + blank.substring(0, blank.length() - 2 - Integer.toString(label).length()) + " nop");
+            newTexts.put(ctx, s1 + "\n" + blank + "fjp $$" + label + "\n" + s2 + "\n$$" + label + blank.substring(0, blank.length() - 2 - Integer.toString(label).length()) + "nop");
         }
         label++;
     }
@@ -190,9 +197,9 @@ public class UCodeGeneratorListener extends MiniCBaseListener {
         String s1;
         if (hasReturnExpr(ctx)) {
             s1 = newTexts.get(ctx.expr());
-            newTexts.put(ctx, s1 + "\nretv");
+            newTexts.put(ctx, blank + s1 + "\nretv");
         } else {
-            newTexts.put(ctx, "ret");
+            newTexts.put(ctx, blank + "ret");
         }
     }
 
@@ -201,43 +208,43 @@ public class UCodeGeneratorListener extends MiniCBaseListener {
         String s1, s2, s3, op1;
         if (isBinaryOperation(ctx)) {
             if (ctx.getChild(0) != ctx.expr(0)) {
-                s1 = newTexts.get(ctx.IDENT());
+                s1 = localMap.get(ctx.getChild(0).getText());
                 s2 = newTexts.get(ctx.expr(0));
-                newTexts.put(ctx, s2 + "\n" + "str " + s1);
+                newTexts.put(ctx, s2 + "\n" + blank + "str " + s1);
             } else {
                 s1 = newTexts.get(ctx.expr(0));
                 s2 = newTexts.get(ctx.expr(1));
                 op1 = forBinaryOperation(ctx.getChild(1).getText());
-                newTexts.put(ctx, s1 + "\n" + s2 + "\n" + op1);
+                newTexts.put(ctx, s1 + "\n" + s2 + "\n" + blank + op1);
             }
         } else if (isPrefixOperation(ctx)) {
             op1 = forPrefixOperation(ctx.getChild(0).getText());
             s1 = newTexts.get(ctx.expr(0));
-            newTexts.put(ctx, s1 + "\n" + op1);
+            newTexts.put(ctx, s1 + "\n" + blank + op1);
         } else if (isParentheses(ctx)) {
             s1 = newTexts.get(ctx.expr(1));
             newTexts.put(ctx, s1);
         } else if (isIdentOrLiteral(ctx)) {
             if (ctx.getChild(0) == ctx.IDENT()) {
-                s1 = newTexts.get(ctx.IDENT());
-                newTexts.put(ctx, "lod " + s1);
+                s1 = localMap.get(ctx.getChild(0).getText());
+                newTexts.put(ctx, blank + "lod " + s1);
             } else {
                 s1 = ctx.getChild(0).getText();
-                newTexts.put(ctx, "ldc " + s1);
+                newTexts.put(ctx, blank + "ldc " + s1);
             }
         } else if (isArray(ctx)) {
-            s1 = newTexts.get(ctx.IDENT());
+            s1 = localMap.get(ctx.getChild(0).getText());
             s2 = newTexts.get(ctx.expr(0));
-            newTexts.put(ctx, s2 + "\n" + "lda " + s1);
+            newTexts.put(ctx, s2 + "\n" + blank + "lda " + s1);
         } else if (isFunction(ctx)) {
-            s1 = ctx.IDENT().toString();
+            s1 = ctx.getChild(0).getText();
             s2 = newTexts.get(ctx.args());
-            newTexts.put(ctx, s2 + "\n" + "call " + s1);
+            newTexts.put(ctx, s2 + "\n" + blank + "call " + s1);
         } else if (ctx.getChildCount() == 6) {
-            s1 = newTexts.get(ctx.IDENT());
+            s1 = localMap.get(ctx.getChild(0).getText());
             s2 = newTexts.get(ctx.expr(0));
             s3 = newTexts.get(ctx.expr(1));
-            newTexts.put(ctx, s2 + "lda " + s1 + "\nadd\n" + s3 + "sti");
+            newTexts.put(ctx, s2 + blank + "lda " + s1 + "\n" + blank + "add\n" + s3 + blank + "sti");
         }
     }
 
